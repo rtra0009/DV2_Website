7eaf5cacc3163fb47a576f701153344674790c42
rtra0009 <rtra0009@student.monash.edu>
2022-10-17 00:02:17 +1100

    update

--- airbnb_vis.js
+++ airbnb_vis.js
@@ -1,4 +1,29 @@
-var vg_1 = "airbnb_map.vg.json";
-vegaEmbed("#map", vg_1).then(function (result) {
+var vg_1 = "airbnb_avg_price.vg.json";
+vegaEmbed("#average_price_map", vg_1).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "airbnb_room_type.vg.json";
+vegaEmbed("#room_type_pie", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "airbnb_room_type_stacked.vg.json";
+vegaEmbed("#room_type_stacked", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "airbnb_scatterplot.vg.json";
+vegaEmbed("#airbnb_scatterplot", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "airbnb_heatmap.vg.json";
+vegaEmbed("#airbnb_heatmap", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "airbnb_property_type.vg.json";
+vegaEmbed("#airbnb_property_type", vg_6).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
